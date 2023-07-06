b5744b30498ec572fdea5524c73f7bedda1efc05
qnbie <qnbie@pm.me>
2023-07-06 23:14:35 +0200

    Add example topics

--- src/lib/components/TopicCard.svelte
+++ src/lib/components/TopicCard.svelte
@@ -1,10 +1,9 @@
-<div class="card lg:card-side bg-base-100 shadow-xl">
-    <slot name="figure"></slot>
-    <div class="card-body">
-      <h2 class="card-title"><slot name="title"></slot></h2>
-      <slot name="description"></slot>
-      <div class="card-actions justify-end">
-        <slot name="button"></slot>
-      </div>
-    </div>
-  </div>
+<div class="card w-96 bg-base-100 shadow-xl">
+	<div class="card-body">
+		<h2 class="card-title"><slot name="title" /></h2>
+		<slot name="description" />
+		<div class="card-actions justify-end">
+			<slot name="button" />
+		</div>
+	</div>
+</div>
