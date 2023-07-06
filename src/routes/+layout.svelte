af3ae12632848d1f1bfaf421bafd462b17a06832
qnbie <qnbie@pm.me>
2023-07-06 18:33:53 +0200

    Bug fixes for Github Pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,3 @@
 <script lang="ts">
-    import type { LayoutData } from './$types';
-    
     export const prerender = true;
-    export let data: LayoutData;
 </script>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
+import {
+    vitePreprocess
+} from '@sveltejs/kit/vite';
+
+
 
 const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 
 export default {
+	preprocess: vitePreprocess(),
+
     kit: {
         adapter: adapter({
             // default options are shown. On some platforms
